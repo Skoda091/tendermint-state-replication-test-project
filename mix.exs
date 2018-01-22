ae438aee8bdb5deafd09b5c8cf8e757aa982f2e8
Adam Skołuda <vexhelel@gmail.com>
2018-01-22 17:06:02 +0100

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -23,7 +23,9 @@ defmodule TendermintStateReplicationTestProject.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:abci_counter, github: "KrzysiekJ/abci_counter", branch: "master"}
+      {:gpb, git: "https://github.com/tomas-abrahamsson/gpb", tag: "4.0.2"},
+      {:ranch, git: "https://github.com/ninenines/ranch", tag: "1.3.2"},
+      {:abci_server, github: "KrzysiekJ/abci_server", branch: "master"}
     ]
   end
 end
