58f15ec1650f3488dc5b2f39b43eed8f42d4af6a
Adam Skołuda <vexhelel@gmail.com>
2018-01-22 11:48:07 +0100

    Init elixir app

---
+++ lib/tendermint_state_replication_test_project.ex
@@ -0,0 +1,18 @@
+defmodule TendermintStateReplicationTestProject do
+  @moduledoc """
+  Documentation for TendermintStateReplicationTestProject.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TendermintStateReplicationTestProject.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tendermint_state_replication_test_project_test.exs
@@ -0,0 +1,8 @@
+defmodule TendermintStateReplicationTestProjectTest do
+  use ExUnit.Case
+  doctest TendermintStateReplicationTestProject
+
+  test "greets the world" do
+    assert TendermintStateReplicationTestProject.hello() == :world
+  end
+end
