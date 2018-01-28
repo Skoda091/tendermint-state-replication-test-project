e0f460c8e308f464edf723a3c443c60255ec795b
Adam Skołuda <vexhelel@gmail.com>
2018-01-28 17:57:57 +0100

    Rename module to BallService

--- lib/ball_service.ex
+++ lib/ball_service.ex
@@ -1,4 +1,4 @@
-defmodule TendermintStateReplicationTestProject.Request do
+defmodule TendermintStateReplicationTestProject.BallService do
   def pass_ball(from, to) do
     params = "from=#{from}:to=#{to}"
     HTTPoison.get(~s(http://localhost:46657/broadcast_tx_commit?tx="#{params}"), [], [ ssl: [{:versions, [:'tlsv1.2']}] ])
@@ -8,6 +8,3 @@ defmodule TendermintStateReplicationTestProject.Request do
     HTTPoison.get(~s(http://localhost:46657/abci_query?data="participant_who_has_the_ball"), [], [ ssl: [{:versions, [:'tlsv1.2']}] ])
   end
 end
-
-# TendermintStateReplicationTestProject.Request.pass_ball("a", "b")
-# http://localhost:46657/abci_query?path=_&data=
